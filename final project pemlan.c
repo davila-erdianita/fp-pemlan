4f379c2ac52f7559334d93682e319c7f29634be0
dellamayapuspita <davilaerdianita1@gmail.com>
2019-12-12 03:15:47 +0700

    tampilan data

--- final project pemlan.c
+++ final project pemlan.c
@@ -21,4 +21,19 @@ struct data{
 	char jenis[20];
 	int totharga;
 }; typedef struct data data;
+void lihat(int n,struct data a[]){
+	int i;
+				printf("\n+----------------------------------------------+\n");
+				printf("			>>>	Daftar Data Pelanggan<<< \n");
+				printf("\n+----------------------------------------------+\n");
+		printf("\n+-----------------------------------------------------------------------------------------------------------------------------------------+");
+		printf("\n|No.Resi| Nama Penerima | Alamat Penerima | Nama Pengirim | Alamat Pengirim | Berat Barang | Jenis Pengiriman | Total Harga |");
+		printf("\n+-----------------------------------------------------------------------------------------------------------------------------------------+");
+				for(i=0; i<n; i++){
+
+				
+		printf("\n|   %i   |   %s  |	%s no %i kota %s,%s |	%s |	%s no %i kota %s,%s |	%i kg |	%s |	%i |",a[i].resi,a[i].namapenerima,a[i].addr.jalan,a[i].addr.norumah,a[i].addr.kota,a[i].addr.provinsi,a[i].namapengirim,a[i].addr1.jalan,a[i].addr1.norumah,a[i].addr1.kota,a[i].addr1.provinsi,a[i].berat,a[i].jenis,a[i].totharga);
+					
+				}
+}
 
